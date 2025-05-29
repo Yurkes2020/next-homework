0f429c88a6eb73b75ac96bdff7cbaf59af949742
Yurkes2020 <yurkes2020@gmail.com>
2025-05-29 20:07:40 +0300

    hw1 fix types

--- src/app/servises/api.servise.tsx
+++ src/app/servises/api.servise.tsx
@@ -1,31 +1,33 @@
+import {Post, User, Comment} from "@/app/models/types";
+
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
-export const fetchUsers = async () => {
+export const fetchUsers = async ():Promise<User[]> => {
 	const res = await fetch(`${BASE_URL}/users`);
 	return res.json();
 };
 
-export const fetchUser = async (id: string) => {
+export const fetchUser = async (id: string):Promise<User> => {
 	const res = await fetch(`${BASE_URL}/users/${id}`);
 	return res.json();
 };
 
-export const fetchPosts = async () => {
+export const fetchPosts = async ():Promise<Post[]> => {
 	const res = await fetch(`${BASE_URL}/posts`);
 	return res.json();
 };
 
-export const fetchPost = async (id: string) => {
+export const fetchPost = async (id: string):Promise<Post> => {
 	const res = await fetch(`${BASE_URL}/posts/${id}`);
 	return res.json();
 };
 
-export const fetchComments = async () => {
+export const fetchComments = async ():Promise<Comment[]> => {
 	const res = await fetch(`${BASE_URL}/comments`);
 	return res.json();
 };
 
-export const fetchComment = async (id: string) => {
+export const fetchComment = async (id: string):Promise<Comment> => {
 	const res = await fetch(`${BASE_URL}/comments/${id}`);
 	return res.json();
 };
